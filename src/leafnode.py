358db2881736692efd58ca1d604b1e21e5c85426
caaldrid <aldrichsea@outlook.com>
2024-09-11 10:13:21 -0700

    Remove the default None value for the tag and value parameters of the LeafNode init

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,14 +2,17 @@ from htmlnode import HTMLNode
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props=None):
-        if value == None or value == "":
-            raise ValueError()
-        super().__init__(tag=tag, value=value, props=props)
+    def __init__(self, tag, value, props=None):
+            super().__init__(tag=tag, value=value, props=props)
 
     def to_html(self):
+        if self.value == None or self.value == "":
+            raise ValueError()
 
         if self.tag == None:
             return self.value
 
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
