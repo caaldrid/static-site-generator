857a30f7c58d96f3005d7b3a14862dc10d4b8134
caaldrid <aldrichsea@outlook.com>
2024-10-08 14:40:44 -0700

    Add way to tell the block's markdow-block type

--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,6 @@
+import re
+
+
 def markdown_to_blocks(markdown: str):
     # assuming that each block is seperated by a blank lines
     lines = markdown.split("\n\n")
@@ -5,3 +8,27 @@ def markdown_to_blocks(markdown: str):
     blocks = map(lambda line: line.strip(), lines)
 
     return list(blocks)
+
+
+def block_to_block_type(block_text: str):
+
+    if re.fullmatch(r"(?<!.)`{3}(?:\n)?(?:.*\n)+`{3}", block_text) != None:
+        return "code"
+
+    lines = block_text.splitlines()
+
+    if len(lines) == 1 and re.match(r"(?<!.)\#{1,6}\s.+", block_text) != None:
+
+        return "heading"
+
+    for line in lines:
+        if re.match(r"(?<!.)\>\s.+", line) != None:
+            return "quote"
+
+        if re.match(r"(?<!.)[\*\-]\s.+", line) != None:
+            return "unordered_list"
+
+        if re.match(r"(?<!.)\d\.\s.+", line) != None:
+            return "ordered_list"
+
+    return "paragraph"
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blocks import markdown_to_blocks
+from blocks import block_to_block_type, markdown_to_blocks
 
 
 class TestBlock(unittest.TestCase):
@@ -26,3 +26,52 @@ class TestBlock(unittest.TestCase):
 * This is another list item"""
             ),
         )
+
+    def test_block_to_block_type(self):
+        test_md = """# This is a heading
+
+## A subsetting
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+
+- foo
+- bar
+- baz
+
+1. fasd
+1. basr
+1. dasdaf
+
+```
+print("Hello, world!")
+for i in range(10):
+    print(i)
+```
+
+> I am quoting
+> as
+> book"""
+
+        expecting = [
+            "heading",
+            "heading",
+            "paragraph",
+            "unordered_list",
+            "unordered_list",
+            "ordered_list",
+            "code",
+            "quote",
+        ]
+
+        blocks = markdown_to_blocks(test_md)
+        got = list(map(lambda block: block_to_block_type(block), blocks))
+
+        self.assertEqual(got, expecting)
+
+
+if __name__ == "__main__":
+    unittest.main()
