81e829f23c89f4cf16099b67aa72b16f9c9ace16
caaldrid <aldrichsea@outlook.com>
2024-10-10 11:38:58 -0700

    convert markdown blocks to htlm

---
+++ src/markdown_to_html.py
@@ -0,0 +1,80 @@
+import re
+
+from blocks import block_to_block_type, block_types, markdown_to_blocks
+from leafnode import LeafNode
+from parentnode import ParentNode
+from textnode import text_node_to_html_node, text_to_textnodes
+
+
+def markdown_to_html_node(markdown):
+    def create_list_html(txt: str):
+        list_items = txt.splitlines()
+        html_list = []
+
+        for item in list_items:
+            regexp = re.compile(r"^(\*|\-|(?:\d.))")
+
+            list_txt = regexp.sub("", item).strip()
+            inline_nodes = text_to_textnodes(list_txt)
+            if len(inline_nodes) == 0:
+                html_list.append(LeafNode("li", list_txt))
+            else:
+                html_list.append(
+                    ParentNode(
+                        "li",
+                        list(
+                            map(lambda node: text_node_to_html_node(node), inline_nodes)
+                        ),
+                    )
+                )
+
+        return html_list
+
+    blocks = markdown_to_blocks(markdown)
+
+    html_root = ParentNode("div", [])
+
+    if html_root.children is None:
+        raise Exception("Why?")
+
+    for block in blocks:
+        block_type = block_to_block_type(block)
+
+        match block_type:
+            case block_types.heading:
+                heading_level = block.count("#")
+                heading_text = block.lstrip("#").strip()
+                html_root.children.append(LeafNode(f"h{heading_level}", heading_text))
+            case block_types.paragraph:
+                lines = block.split("\n")
+                paragraph = " ".join(lines)
+                inline_nodes = text_to_textnodes(paragraph)
+                html_root.children.append(
+                    ParentNode(
+                        "p",
+                        list(
+                            map(lambda node: text_node_to_html_node(node), inline_nodes)
+                        ),
+                    )
+                )
+            case block_types.code:
+                code_text = block.strip("```").strip()
+                html_root.children.append(
+                    ParentNode("pre", [LeafNode("code", code_text)])
+                )
+            case block_types.quote:
+                quote_text = " ".join(
+                    list(
+                        map(
+                            lambda quote_line: quote_line.lstrip(">").strip(),
+                            block.splitlines(),
+                        )
+                    )
+                )
+                html_root.children.append(LeafNode("blockquote", quote_text))
+            case block_types.ulist:
+                html_root.children.append(ParentNode("ul", create_list_html(block)))
+            case block_types.olist:
+                html_root.children.append(ParentNode("ol", create_list_html(block)))
+
+    return html_root
---
+++ src/test_markdown_to_html.py
@@ -0,0 +1,125 @@
+import unittest
+
+from markdown_to_html import markdown_to_html_node
+
+
+class TestMarkdownToHTML(unittest.TestCase):
+    def test_markdown_to_html_node(self):
+        test_md = """# This is a heading
+
+## A subsetting
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+
+- foo
+- bar
+- baz
+
+1. fasd
+1. basr
+1. dasdaf
+
+```
+print("Hello, world!")
+for i in range(10):
+    print(i)
+```
+
+> I am quoting
+> a
+> book"""
+        expected = '<div><h1>This is a heading</h1><h2>A subsetting</h2><p>This is a paragraph of text. It has some <b>bold</b> and <i>italic</i> words inside of it.</p><ul><li>This is the first list item in a list block</li><li>This is a list item</li><li>This is another list item</li></ul><ul><li>foo</li><li>bar</li><li>baz</li></ul><ol><li>fasd</li><li>basr</li><li>dasdaf</li></ol><pre><code>print("Hello, world!")\nfor i in range(10):\n    print(i)</code></pre><blockquote>I am quoting a book</blockquote></div>'
+        got = markdown_to_html_node(test_md).to_html()
+
+        self.assertEqual(got, expected)
+
+    def test_paragraph(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>",
+        )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with *italic* text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_lists(self):
+        md = """
+- This is a list
+- with items
+- and *more* items
+
+1. This is an `ordered` list
+2. with items
+3. and more items
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ul><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>",
+        )
+
+    def test_headings(self):
+        md = """
+# this is an h1
+
+this is paragraph text
+
+## this is an h2
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>this is an h1</h1><p>this is paragraph text</p><h2>this is an h2</h2></div>",
+        )
+
+    def test_blockquote(self):
+        md = """
+> This is a
+> blockquote block
+
+this is paragraph text
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a blockquote block</blockquote><p>this is paragraph text</p></div>",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
